fd518214bbf23cd358078f0b9bcaabf62b692e5a
Kyle Mendell <kmendell@ofkm.us>
2025-01-06 13:34:20 -0600

    added comments

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 func main() {
+	// Recreated Main function in main.go and run both other main functions.
+	// If the go routines hold up this should work till the cmd is exited
 	rigmain()
 	wsjtxmain()
 }
